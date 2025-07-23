765748846805d688fe0b73efc929281d7c3f3b86
omarelkhaoudi <omarelkhaoudi7@gmail.com>
2025-07-23 05:03:13 +0200

    fix the home page in the app

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Body from './components/Body';
 import PostPage from './PostPage';
 import Login from './pages/Login';
-import Home from './pages/Home'
+import Home from './pages/Home';
 import NotFound from './pages/NotFound'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Body />} />
+        <Route path="/" element={<Home />} />
         <Route path="/post/:id" element={<PostPage />} />
         <Route path='/login' element={<Login/>} />
       </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-// import Navbar from '../components/Navbar'
+import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
-// import Body from '../components/Body'
+import Body from '../components/Body'
 import Contact from '../components/Contact'
 // import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <div>
+      <Navbar/>
       <Hero />
+      <Body/>
       <Contact />
     </div>
   )
