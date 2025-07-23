a366309145408461b402b694df5737d6df119d83
ELITEBOOK <amranleila3@gmail.com>
2025-07-23 04:43:09 +0200

    puch laila

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import NotFound from './pages/NotFound'
+
+import Footer from "../src/components/Footer";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </Router>
-  )
+    <div>
+     
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+
+export default App;
+
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import "../App.css"; // Import CSS for Footer component
+import { FaInstagram, FaFacebookF, FaTwitter, FaEnvelope } from "react-icons/fa";
 
-const Footer = () => {
+export default function Footer() {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="footer-top">
+        <div className="footer-brand">
+          <h3>📍 Maroc Découverte</h3>
+          <p>
+            Votre guide authentique pour explorer les merveilles du Maroc.
+            De l’Atlas au Sahara, découvrez la magie du royaume chérifien
+            à travers nos récits passionnés et nos conseils d’experts.
+          </p>
+          <div className="social-icons">
+            <FaInstagram />
+            <FaFacebookF />
+            <FaTwitter />
+            <FaEnvelope />
+          </div>
+        </div>
 
-export default Footer
+        <div className="footer-nav">
+          <h4>Navigation</h4>
+          <ul>
+            <li>Accueil</li>
+            <li>À propos</li>
+            <li>Blog</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+
+        <div className="footer-destinations">
+          <h4>Destinations Maroc</h4>
+          <ul>
+            <li>Marrakech</li>
+            <li>Casablanca</li>
+            <li>Fès</li>
+            <li>Chefchaouen</li>
+          </ul>
+        </div>
+
+        <div className="footer-experiences">
+          <h4>Expériences</h4>
+          <ul>
+            <li>Atlas & Montagnes</li>
+            <li>Sahara & Désert</li>
+            <li>Côtes Atlantique</li>
+            <li>Médinas & Culture</li>
+          </ul>
+        </div>
+      </div>
+
+      
+    </footer>
+  );
+}
