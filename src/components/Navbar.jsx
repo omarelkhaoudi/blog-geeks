cab56c258109d828f783455d2dc9cef057fd0a8a
Moufid-admin <walidmoufid11@gmail.com>
2025-07-23 05:38:22 +0100

    fix navbar error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,30 @@
-// Navbar.js
 import React from "react";
 import { Link } from "react-router-dom";
-import './Navbar.css'; // Optionnel, si tu veux ajouter du style
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <h2>Voyage au Maroc</h2>
-      <ul>
-        <li><Link to="/">Accueil</Link></li>
-        <li><Link to="/a-propos">À propos</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+    <nav className="fixed top-0 left-0 w-full z-30 bg-white shadow-md px-6 py-4 flex justify-between items-center">
+      <h2 className="text-2xl font-bold text-[#002f5f]">HackaDeur Blog</h2>
+      <ul className="flex space-x-6 text-[#002f5f] font-medium items-center">
+        <li>
+          <Link
+            to="/"
+            className="hover:text-[#014080] transition duration-200"
+          >
+            Accueil
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/login"
+            className="bg-[#002f5f] text-white px-4 py-2 rounded-md font-semibold hover:bg-[#014080] transition duration-200"
+          >
+            Login
+          </Link>
+        </li>
       </ul>
     </nav>
   );
 };
 
-export default Navbar;
+export default Navbar;
